e506ada9dd43f9dff2322ef36f2f9f2fb339785c
Soedipo <dipo.lendou@gmail.com>
2023-02-28 19:30:28 +0700

    Add laravel 9 version

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/service', function () {
+    return view('service');
+});
+
+Route::get('/destination', function () {
+    return view('destination');
+});
+
+Route::get('/guide', function () {
+    return view('guide');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/package', function () {
+    return view('package');
+});
+
+Route::get('/single', function () {
+    return view('single');
+});
