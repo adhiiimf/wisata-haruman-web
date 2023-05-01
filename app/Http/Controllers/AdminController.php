73985847384f2598947469c5984b211f9e77c548
Adhi Muhammad Fahmi <adhimfahmi@gmail.com>
2023-05-02 02:05:48 +0700

    add authorize and logout admin user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,8 +9,47 @@ use Illuminate\Support\Facades\Auth;
 class AdminController extends Controller
 {
     // function to login page of admin role
-    public function login(){
+    public function loginpage() {
         return view('auth.admin.login');
+    }
+
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function authcheck(Request $request)
+    {
+        // validate form of email and password fields
+        $this->validate($request, [
+			'email'      => ['string','required','email'],
+			'password'  => ['string','required','min:5'],
+		]);
+		
+		// Attempt to log the user in
+		if (Auth::guard('admins')->attempt(
+			['email' => $request->email, 'password' => $request->password], true)) {
+            $request->session()->regenerate();
+			
+            return redirect('/adminDashboard');
+		}
+        
+        // if not authorize, redirect to login with error message
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function logout() {
+        Auth::guard('admins')->logout();
+        
+        $request->session()->invalidate();
+ 
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
     
 }
