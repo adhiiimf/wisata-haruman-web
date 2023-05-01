f4572f1bd2a25531525c9f7529df81881facef87
Adhi Muhammad Fahmi <adhimfahmi@gmail.com>
2023-05-02 02:07:23 +0700

    add prototype tes for login and homebase of admin

--- resources/views/auth/admin/Login.blade.php
+++ resources/views/auth/admin/Login.blade.php
@@ -7,6 +7,28 @@
     <title>Admin - Login</title>
 </head>
 <body>
-    <h1>Login</h1>
+    <form method="POST" action="{{ route('auth.admin.logincheck') }}">
+        @csrf
+        <h1 class="">Admin Login</h1>
+
+        <div class="">
+            <input type="text" class="form-control" name="email" placeholder="email" required="required" autofocus>
+            <label for="floatingName">Email or Username</label>
+            @if ($errors->has('email'))
+                <span class="">{{ $errors->first('email') }}</span>
+            @endif
+        </div>
+        
+        <div class="">
+            <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+            <label for="floatingPassword">Password</label>
+            @if ($errors->has('password'))
+                <span class="">{{ $errors->first('password') }}</span>
+            @endif
+        </div>
+
+        <button class="" type="submit">Login</button>
+        
+    </form>
 </body>
 </html>
