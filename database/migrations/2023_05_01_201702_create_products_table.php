7e64db03d084a205f095ae670bc2c9e1724786af
Adhi Muhammad Fahmi <adhimfahmi@gmail.com>
2023-05-02 04:18:20 +0700

    add DB Relation v1

---
+++ database/migrations/2023_05_01_201702_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('productTitle');
+            $table->string('productImage');
+            $table->string('description');
+            $table->string('stocks');
+            $table->string('phoneNumber');
+            $table->unsignedBigInteger('admins_id');
+            $table->foreign('admins_id')->references('id')->on('admins')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
