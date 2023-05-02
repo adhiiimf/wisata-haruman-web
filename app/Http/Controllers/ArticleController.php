4702aa71b55cf94adc9ed31f95e0663dca78ffd3
Adhi Muhammad Fahmi <adhimfahmi@gmail.com>
2023-05-02 21:27:24 +0700

    add create article content

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,10 +1,58 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ArticleController extends Controller
 {
-    //
+    public function postArticle(Request $request)
+    {
+        $admins_id = Auth::user()->first();
+        $this->validate($request, [
+             'articleTitle' => 'required',
+             'articleImage' => ['required','mimes:png,jpg,jpeg','max:2048'],
+             'content' => 'required',
+            //  'admins_id' => 'required'
+        ]);
+
+        // for each content and images on summarnote editor
+        $content = $request->content;
+        $dom = new \DomDocument();
+        $dom->loadHtml($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        $imageFile = $dom->getElementsByTagName('img');
+
+        foreach($imageFile as $item => $image){
+            $data = $image->getAttribute('src');
+            list($type, $data) = explode(';', $data);
+            list(, $data)      = explode(',', $data);
+            $imgeData = base64_decode($data);
+            $image_name= "/articles/" . time().$item.'.png';
+            $path = public_path() . $image_name;
+            file_put_contents($path, $imgeData);
+            
+            $image->removeAttribute('src');
+            $image->setAttribute('src', $image_name);
+            }
+            
+            // for thumbnail image
+        if($request->file('articleImage')){
+            $file= $request->file('articleImage');
+            $filename= time().$file->getClientOriginalName();
+            $file-> move(public_path('/articles'), $filename);
+            $thumPath = '/articles/'. $filename;
+        }
+
+        $content = $dom->saveHTML();
+        $post = Article::create([
+                'articleTitle' => $request->articleTitle,
+                'articleImage' => $thumPath,
+                'content' => $content,
+                'admins_id' => $admins_id['id']
+        ]);
+
+        // log result
+        dd($post->toArray());
+    }
 }
