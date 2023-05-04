b252a2896e50e1a0229197aaa960b4d11187c1a1
Adhi Muhammad Fahmi <adhimfahmi@gmail.com>
2023-05-04 18:29:45 +0700

    create post and travel routes

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -1,10 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Travel;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class TravelController extends Controller
 {
-    //
+    public function postTravel(Request $request)
+    {
+        $admins_id = Auth::user()->first();
+        $this->validate($request, [
+             'travelTitle' => 'required',
+             'travelImg-1' => ['required','mimes:png,jpg,jpeg','max:2048'],
+             'description' => 'required',
+             'travelStatus' => 'required',
+             'phoneNumber' => 'required'
+            //  'admins_id' => 'required'
+        ]);   
+            // for thumbnail image
+        if($request->file('travelImg-1')){
+            $file= $request->file('travelImg-1');
+            $filename= time().$file->getClientOriginalName();
+            $file-> move(public_path('/travels'), $filename);
+            $thumPath = '/travels/'. $filename;
+        }
+
+        $post = Travel::create([
+                'travelTitle' => $request->travelTitle,
+                'travelImg-1' => $thumPath,
+                'description' => $request->description,
+                'travelStatus' => $request->travelStatus,
+                'phoneNumber' => $request->phoneNumber,
+                'admins_id' => $admins_id['id']
+        ]);
+        
+        echo $post;
+        // log result
+        dd($post->toArray());
+    }
 }
