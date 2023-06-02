fe786881bb8dff2ea4a1e6ea995ec1702f1c7620
adhiiimf <adhimfahmi@gmail.com>
2023-06-02 23:08:18 +0700

    add request url for product click

--- resources/views/Main.blade.php
+++ resources/views/Main.blade.php
@@ -217,7 +217,7 @@
                 <div class="swiper-wrapper pb-20">
                     @foreach ($product_data as $product)
                         <div class="swiper-slide !flex !justify-center">
-                            <a href="">
+                            <a href="/product/{{$product->id}}">
                                 <div class="relative flex text-center items-center justify-center w-72 h-56 bg-violet-800 bg-cover bg-center rounded-2xl brightness-90 max-sm:w-52 max-sm:h-48" style="background-image: url('{{$product->productImage}}')">
                                     <span class="text-2xl flex relative !brightness-100 bg-black w-full py-2 bg-opacity-30 text-center justify-center items-center">
                                         {{$product->productTitle}}
