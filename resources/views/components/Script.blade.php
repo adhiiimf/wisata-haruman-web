cf1da7649c9ec833415b21bfba3c080a80edea08
adhiiimf <adhimfahmi@gmail.com>
2023-06-04 15:30:15 +0700

    clear whitespace

--- resources/views/components/Script.blade.php
+++ resources/views/components/Script.blade.php
@@ -2,58 +2,58 @@
 <script src="js/app.js"></script>
 
 <script>
-    var map = L.map('map').setView([-7.072505262549012, 107.94232816547856], 13);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-    var marker = L.marker([-7.072505262549012, 107.94232816547856]).addTo(map);
+        var map = L.map('map').setView([-7.072505262549012, 107.94232816547856], 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+        var marker = L.marker([-7.072505262549012, 107.94232816547856]).addTo(map);
 
-    var swiper = new Swiper(".videoSwiper", {
-    loop: true,
-    autoplay: {
-        delay: 4000,
-    },
-    spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-  });
-  var swiper1 = new Swiper(".travelSwiper", {
-        effect: "coverflow",
-        slidesPerView: 3,
-        spaceBetween: 40,
-        autoplay: true,
-        centeredSlides: true,
-        coverflowEffect: {
-            rotate: 20,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: false,
+        var swiper = new Swiper(".videoSwiper", {
+        loop: true,
+        autoplay: {
+            delay: 4000,
         },
+        spaceBetween: 30,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
         },
-    });
-    var swiper2 = new Swiper(".productSwiper", {
-        effect: "coverflow",
-        slidesPerView: 3,
-        spaceBetween: 40,
-        autoplay: true,
-        centeredSlides: true,
-        coverflowEffect: {
-            rotate: 20,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: false,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-    });
+        });
+        var swiper1 = new Swiper(".travelSwiper", {
+            effect: "coverflow",
+            slidesPerView: 3,
+            spaceBetween: 40,
+            autoplay: true,
+            centeredSlides: true,
+            coverflowEffect: {
+                rotate: 20,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+        var swiper2 = new Swiper(".productSwiper", {
+            effect: "coverflow",
+            slidesPerView: 3,
+            spaceBetween: 40,
+            autoplay: true,
+            centeredSlides: true,
+            coverflowEffect: {
+                rotate: 20,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
 </script>
